76e6f02dc4778ae06bbd05188b03bf89196965a1
fwcd <fwcdmail@gmail.com>
2022-07-07 12:16:00 +0200

    Bootstrap a bunch of compiler services

--- src/main/kotlin/dev/fwcd/kas/KotlinLanguageServer.kt
+++ src/main/kotlin/dev/fwcd/kas/KotlinLanguageServer.kt
@@ -1,10 +1,24 @@
 package dev.fwcd.kas
 
+import com.intellij.mock.MockApplication
+import com.intellij.mock.MockProject
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.util.Disposer
 import org.eclipse.lsp4j.InitializeParams
 import org.eclipse.lsp4j.InitializeResult
 import org.eclipse.lsp4j.ServerCapabilities
 import org.eclipse.lsp4j.ServerInfo
 import org.eclipse.lsp4j.services.*
+import org.jetbrains.kotlin.analysis.api.standalone.configureApplicationEnvironment
+import org.jetbrains.kotlin.analysis.api.standalone.configureProjectEnvironment
+import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
+import org.jetbrains.kotlin.cli.common.config.addKotlinSourceRoots
+import org.jetbrains.kotlin.cli.common.messages.MessageCollector
+import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import java.net.URI
+import java.nio.file.Path
 import java.util.concurrent.CompletableFuture
 
 /**
@@ -22,12 +36,44 @@ class KotlinLanguageServer: LanguageServer, LanguageClientAware {
     private var client: LanguageClient? = null
 
     override fun initialize(params: InitializeParams?): CompletableFuture<InitializeResult> {
+        // TODO: Investigate proper lifecycle management with disposables (should we store a Disposable in the class?)
+        // TODO: Make source-resolution more flexible (currently only Gradle-style src/main/kotlin folders are considered)
+        // TODO: Add proper message collector (e.g. one that logs the messages)?
+
+        // Locate sources
+        val workspaceFolders = params?.workspaceFolders ?: listOf()
+        val sourceRoots = workspaceFolders
+            .map { Path.of(URI(it.uri)).resolve("src").resolve("main").resolve("kotlin").toString() }
+
+        // Configure Kotlin compiler
+        val compilerConfig = CompilerConfiguration()
+        compilerConfig.addKotlinSourceRoots(sourceRoots)
+        compilerConfig.put(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, MessageCollector.NONE)
+
+        // Bootstrap Kotlin compiler application environment for standalone analysis mode
+        val application = ApplicationManager.getApplication() as MockApplication
+        configureApplicationEnvironment(application)
+
+        // Bootstrap Kotlin core environment for standalone analysis mode
+        val coreEnv = KotlinCoreEnvironment.createForProduction(
+            Disposer.newDisposable(),
+            compilerConfig,
+            EnvironmentConfigFiles.JVM_CONFIG_FILES
+        )
+        configureProjectEnvironment(
+            coreEnv.project as MockProject,
+            compilerConfig,
+            coreEnv::createPackagePartProvider
+        )
+
+        // Assemble LSP initialization response
         val result = InitializeResult(
             ServerCapabilities().apply {
                 // TODO
             },
-            ServerInfo("Kotlin Analyzer Server")
+            ServerInfo("Kotlin Analysis Server")
         )
+
         return CompletableFuture.completedFuture(result)
     }
 
